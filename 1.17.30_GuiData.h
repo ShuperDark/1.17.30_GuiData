18751d2e0add244c9a1800063bb785cdcb3aa692
ShuperDark <darkshuper6.like@gmail.com>
2021-09-25 18:38:29 +0900

    Add files via upload

---
+++ 1.17.30_GuiData.h
@@ -0,0 +1,157 @@
+
+GuiData::getGuiScaleIndexForSmallScreen(Vec2 const&)
+0x1003312e0
+GuiData::getGuiScaleIndexForLargeScreen(Vec2 const&)
+0x100331368
+GuiData::getGuiScaleIndexForSplitscreenConsole(Vec2 const&)
+0x1003313f0
+GuiData::getGuiScaleIndexForHandheldConsole(Vec2 const&)
+0x100331478
+GuiData::resolveGuiScale(int)
+0x100331500
+GuiData::GuiData(IClientInstance&)
+0x100331520
+GuiData::tick()
+0x100331b08
+GuiData::_tickItemDrop()
+0x100331cd8
+GuiData::showPopupNotice(ItemStackBase const&)
+0x100331f60
+GuiData::_tickDelayedMessages()
+0x100332114
+GuiData::getNumSlots() const
+0x1003322ec
+GuiData::_addMessage(GuiMessage const&, ProfanityFilterContext)
+0x1003322f4
+GuiData::isMuteChat() const
+0x100332700
+GuiMessage::getTTSMessage() const
+0x100332708
+GuiData::_addMessage(GuiMessage::MessageType, std::__ndk1::basic_string<char, std::__ndk1::char_traits<char>, std::__ndk1::allocator<char> > const&, std::__ndk1::basic_string<char, std::__ndk1::char_traits<char>, std::__ndk1::allocator<char> > const&, std::__ndk1::basic_string<char, std::__ndk1::char_traits<char>, std::__ndk1::allocator<char> > const&, float, bool, std::__ndk1::basic_string<char, std::__ndk1::char_traits<char>, std::__ndk1::allocator<char> > const&, std::__ndk1::basic_string<char, std::__ndk1::char_traits<char>, std::__ndk1::allocator<char> > const&, ProfanityFilterContext)
+0x100332710
+
+GuiData::getDevConsoleScreenMessages()
+0x1003334f0
+GuiData::getLastDevConsoleMessage()
+0x1003334f8
+
+GuiData::getContentLogMessages() const
+0x100333d40
+GuiData::getLastContentLogMessage() const
+0x100333d48
+GuiData::displayClientMessage(std::__ndk1::basic_string<char, std::__ndk1::char_traits<char>, std::__ndk1::allocator<char> > const&)
+0x100333d64
+GuiData::displayChatMessage(std::__ndk1::basic_string<char, std::__ndk1::char_traits<char>, std::__ndk1::allocator<char> > const&, std::__ndk1::basic_string<char, std::__ndk1::char_traits<char>, std::__ndk1::allocator<char> > const&, std::__ndk1::basic_string<char, std::__ndk1::char_traits<char>, std::__ndk1::allocator<char> > const&, std::__ndk1::basic_string<char, std::__ndk1::char_traits<char>, std::__ndk1::allocator<char> > const&)
+0x100333f40
+GuiData::displayLocalizableMessage(std::__ndk1::basic_string<char, std::__ndk1::char_traits<char>, std::__ndk1::allocator<char> > const&, std::__ndk1::vector<std::__ndk1::basic_string<char, std::__ndk1::char_traits<char>, std::__ndk1::allocator<char> >, std::__ndk1::allocator<std::__ndk1::basic_string<char, std::__ndk1::char_traits<char>, std::__ndk1::allocator<char> > > > const&)
+0x10033404c
+GuiData::displayLocalizedMessage(std::__ndk1::basic_string<char, std::__ndk1::char_traits<char>, std::__ndk1::allocator<char> > const&)
+0x100334278
+GuiData::displaySystemMessage(std::__ndk1::basic_string<char, std::__ndk1::char_traits<char>, std::__ndk1::allocator<char> > const&, std::__ndk1::basic_string<char, std::__ndk1::char_traits<char>, std::__ndk1::allocator<char> > const&, ProfanityFilterContext)
+0x100334454
+GuiData::displayWhisperMessage(std::__ndk1::basic_string<char, std::__ndk1::char_traits<char>, std::__ndk1::allocator<char> > const&, std::__ndk1::basic_string<char, std::__ndk1::char_traits<char>, std::__ndk1::allocator<char> > const&, std::__ndk1::basic_string<char, std::__ndk1::char_traits<char>, std::__ndk1::allocator<char> > const&, std::__ndk1::basic_string<char, std::__ndk1::char_traits<char>, std::__ndk1::allocator<char> > const&)
+0x100334630
+GuiData::displayTextObjectMessage(TextObjectRoot const&)
+0x100334ad8
+GuiData::displayTextObjectWhisperMessage(TextObjectRoot const&, std::__ndk1::basic_string<char, std::__ndk1::char_traits<char>, std::__ndk1::allocator<char> > const&, std::__ndk1::basic_string<char, std::__ndk1::char_traits<char>, std::__ndk1::allocator<char> > const&)
+0x100334c9c
+GuiData::displayAnnouncementMessage(std::__ndk1::basic_string<char, std::__ndk1::char_traits<char>, std::__ndk1::allocator<char> > const&, std::__ndk1::basic_string<char, std::__ndk1::char_traits<char>, std::__ndk1::allocator<char> >, std::__ndk1::basic_string<char, std::__ndk1::char_traits<char>, std::__ndk1::allocator<char> > const&, std::__ndk1::basic_string<char, std::__ndk1::char_traits<char>, std::__ndk1::allocator<char> > const&)
+0x100334e60
+GuiData::handleClick()
+0x100334fb4
+GuiData::getScreenSizeData() const
+0x100335134
+GuiData::showTipMessage(std::__ndk1::basic_string<char, std::__ndk1::char_traits<char>, std::__ndk1::allocator<char> > const&)
+0x10033513c
+GuiData::getTipMessage(std::__ndk1::basic_string<char, std::__ndk1::char_traits<char>, std::__ndk1::allocator<char> >&)
+0x1003351c8
+GuiData::showPopupNotice(std::__ndk1::basic_string<char, std::__ndk1::char_traits<char>, std::__ndk1::allocator<char> > const&, std::__ndk1::basic_string<char, std::__ndk1::char_traits<char>, std::__ndk1::allocator<char> > const&)
+0x100335240
+GuiData::showJukeboxPopupNotice(std::__ndk1::basic_string<char, std::__ndk1::char_traits<char>, std::__ndk1::allocator<char> > const&, std::__ndk1::basic_string<char, std::__ndk1::char_traits<char>, std::__ndk1::allocator<char> > const&)
+0x1003352b8
+GuiData::getNewPopupNotice(std::__ndk1::basic_string<char, std::__ndk1::char_traits<char>, std::__ndk1::allocator<char> >&, std::__ndk1::basic_string<char, std::__ndk1::char_traits<char>, std::__ndk1::allocator<char> >&)
+0x1003352f8
+GuiData::getNewJukeboxPopupNotice(std::__ndk1::basic_string<char, std::__ndk1::char_traits<char>, std::__ndk1::allocator<char> >&, std::__ndk1::basic_string<char, std::__ndk1::char_traits<char>, std::__ndk1::allocator<char> >&)
+0x100335364
+GuiData::setShowProgress(bool)
+0x1003353d0
+GuiData::setScreenSizeData(ScreenSizeData const&)
+0x1003353d8
+GuiData::screenSizeDataValid() const
+0x1003353f4
+GuiData::floorAlignToScreenPixel(float) const
+0x1003353fc
+GuiData::getGuiScale() const
+0x100335414
+GuiData::getInvGuiScale() const
+0x10033541c
+GuiData::ceilAlignToScreenPixel(float) const
+0x100335424
+GuiData::ceilToGuiScale(Vec2 const&) const
+0x100335458
+GuiData::setTouchToolbarArea(RectangleArea const&)
+0x1003354a8
+GuiData::getMessageList()
+0x1003357f4
+GuiData::getLastChatMessage()
+0x1003358ec
+GuiMessage::isForceVisible() const
+0x10033591c
+GuiMessage::getString() const
+0x100335924
+GuiData::getTitleMessage() const
+0x10033592c
+GuiData::getServerSettings()
+0x1003359a0
+GuiData::getServerSettingsId()
+0x1003359a8
+GuiData::postError(int)
+0x1003359b0
+GuiData::updatePointerLocation(short, short)
+0x100335e58
+GuiData::setGuiScale(float)
+0x100335e64
+GuiData::isOddGuiScale() const
+0x100335e74
+GuiData::toggleMuteChat()
+0x100335e98
+GuiData::setMuteChat(bool)
+0x100335ea8
+GuiData::onConfigChanged(Config const&)
+0x100335eb0
+GuiData::onLevelGenerated()
+0x100335eb8
+GuiData::getRcFeedbackOuter()
+0x100335ebc
+GuiData::getRcFeedbackInner()
+0x100335ec4
+GuiData::getVignette()
+0x100335ecc
+GuiData::getCursorMat() const
+0x100335ed4
+GuiData::setMenuPointerPressed(bool)
+0x100335edc
+GuiData::getPointerX() const
+0x100335ee4
+GuiData::getPointerY() const
+0x100335eec
+GuiData::getShowProgress() const
+0x100335ef4
+GuiData::wasToolbarClicked() const
+0x100335efc
+GuiData::getCurrentDropTicks() const
+0x100335fcc
+GuiData::getCurrentDropSlot() const
+0x100335fd4
+GuiData::setLastSelectedSlot(int, ContainerID)
+0x100335fdc
+GuiData::flashSlot(int)
+0x100335fec
+GuiData::setTextToSpeechEnabled(bool)
+0x100336014
+GuiData::setTitle(std::__ndk1::basic_string<char, std::__ndk1::char_traits<char>, std::__ndk1::allocator<char> > const&)
+0x100336130
+GuiData::setSubtitle(std::__ndk1::basic_string<char, std::__ndk1::char_traits<char>, std::__ndk1::allocator<char> > const&)
+0x100336164
+GuiData::setTitleAnimationTimes(int, int, int)
+0x1003361a0
